45a6449407845aae8ffdac9901ab796175541608
范云翼 <fanyunyimac@fanyunyimacdemac.local>
2018-10-26 10:23:41 +0800

    基本完成

--- SixWeekWork/ViewController.swift
+++ SixWeekWork/ViewController.swift
@@ -13,6 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        let redView = UIView()
+//        redView.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+//        let greenView = UIView()
+//        greenView.backgroundColor = #colorLiteral(red: 0.4232724905, green: 1, blue: 0.4650636315, alpha: 1)
+//        let blueView = UIView()
+//        blueView.backgroundColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+//        //stackView内部布局
+//        let stackView = UIStackView(arrangedSubviews: [redView,greenView,blueView])
+//        stackView.axis = .vertical
+//        stackView.alignment = .fill
+//        stackView.distribution = .fillEqually
+//        stackView.spacing = 20
+//        //stackView的约束
+//        stackView.translatesAutoresizingMaskIntoConstraints = false
+//        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20)
+//        stackView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20)
+//        stackView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20)
+//        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+//        view.addSubview(stackView)
     }
 
     override func didReceiveMemoryWarning() {
